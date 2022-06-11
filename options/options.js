eda60b17b934f5afda5dfa4c2e1a27c0f2d32801
Crowdbotics <team@crowdbotics.com>
2022-06-11 18:25:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "black_silence_35681",
+  url: "https://black_silence_35681.botics.co",
+  api: "https://black_silence_35681.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
